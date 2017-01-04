5e9099458ca69095e9cb1000941a1cc7e8bfdacf
ncou <nicolas_courade@yahoo.fr>
2017-01-04 14:47:46 +0100

    improve default palette values + change the arrow positionning

--- spectrum.js
+++ spectrum.js
@@ -56,7 +56,11 @@
         palette: [
             ["rgb(0, 0, 0)", "rgb(34, 34, 34)", "rgb(68, 68, 68)", "rgb(102, 102, 102)", "rgb(136, 136, 136)", "rgb(170, 170, 170)", "rgb(204, 204, 204)", "rgb(238, 238, 238)", "rgb(255, 255, 255)", "transparent"],
             ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(168, 39, 107)"],
-            ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)", "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)", "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)", "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
+            ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)"],
+            ["rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)"],
+            ["rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)"],
+            ["rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)", "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)"],
+            ["rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
         ],
         selectionPalette: [],
         disabled: false
@@ -85,7 +89,7 @@
                 "<div class='sp-palette-container'>",
                     "<div class='sp-palette sp-thumb sp-cf'></div>",
                     "<div class='sp-palette-button-container sp-cf'>",
-                        "<button type='button' class='sp-palette-toggle' title='Options'><img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAYCAYAAAAVibZIAAABaklEQVRIS7WV4TEEQRCFv4sAESACRIAIEAEiQCSIwMngRIAMiAARIALqu5rZ6p3b2Vt3rv907c72m+7Xr3tHrMBGAzHPgE3gAxjPixkCug58JqAvYBvQV60EFaAMMMu7gHBeZGuM1sRFUA8fgS3gBrgFdoHr5DPuC3AF6C+AS+AdOMzAEVSuTufx1XNuEl5ABD1ImS6Ka6ZPJajPvtxfAPUZMKmplY2SS3mK9po4ljc5tnE7xTdN6SWojXoDcjc9fwCOOzKfAEfhfUtqZmqQDeoK3qhoMmo33ullE0F/KhxatuXWTEmVNDSc1kAN2lsUVOItPXKUsf5avj0YlxNlh9dCdnJ0MqBR32kSp6M6RFLS4LTps6RKrquSWkb8Do0TNSP+lYzpsgvlPk1bi1MFbRl59TmycqePelS/biM51ufVZ6WdjZpZuOn2viUtj63lPvR3EqVW026jvCGgfuyASIvg//Lj65nU7qNftTZPxvbjYYkAAAAASUVORK5CYII='></button>",
+                        "<button type='button' class='sp-palette-toggle' title='Options'><img class='' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAYCAYAAAAVibZIAAABaklEQVRIS7WV4TEEQRCFv4sAESACRIAIEAEiQCSIwMngRIAMiAARIALqu5rZ6p3b2Vt3rv907c72m+7Xr3tHrMBGAzHPgE3gAxjPixkCug58JqAvYBvQV60EFaAMMMu7gHBeZGuM1sRFUA8fgS3gBrgFdoHr5DPuC3AF6C+AS+AdOMzAEVSuTufx1XNuEl5ABD1ImS6Ka6ZPJajPvtxfAPUZMKmplY2SS3mK9po4ljc5tnE7xTdN6SWojXoDcjc9fwCOOzKfAEfhfUtqZmqQDeoK3qhoMmo33ullE0F/KhxatuXWTEmVNDSc1kAN2lsUVOItPXKUsf5avj0YlxNlh9dCdnJ0MqBR32kSp6M6RFLS4LTps6RKrquSWkb8Do0TNSP+lYzpsgvlPk1bi1MFbRl59TmycqePelS/biM51ufVZ6WdjZpZuOn2viUtj63lPvR3EqVW026jvCGgfuyASIvg//Lj65nU7qNftTZPxvbjYYkAAAAASUVORK5CYII='></button>",
                     "</div>",
                 "</div>",
                 "<div class='sp-picker-container'>",
@@ -631,7 +635,7 @@
 
 
 
-            
+
             boundElement.trigger('show.spectrum', [ colorOnShow ]);
         }
 
@@ -1003,8 +1007,8 @@
         console.log(dpHeight)
 
         offsetTop -= 154;
-        offsetLeft += inputWidth + 15;
-        //offsetLeft -= dpWidth + 15;
+        offsetLeft += inputWidth + 10;
+        //offsetLeft -= dpWidth + 10;
 
         return {
             top: offsetTop,
@@ -2299,7 +2303,7 @@
         return false;
     }
 
-    window.tinycolor = tinycolor;
+    if (!window.tinycolor) window.tinycolor = tinycolor;
     })();
 
     $(function () {
